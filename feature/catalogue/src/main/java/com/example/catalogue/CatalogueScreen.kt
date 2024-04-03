62132911354e15efb6238b489f4509c1c184cb89
robarcoo <111498279+robarcoo@users.noreply.github.com>
2024-04-03 20:57:41 +0300

    filter counter works correctly

--- feature/catalogue/src/main/java/com/example/catalogue/CatalogueScreen.kt
+++ feature/catalogue/src/main/java/com/example/catalogue/CatalogueScreen.kt
@@ -5,6 +5,7 @@ import android.graphics.Paint.Align
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -48,7 +50,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -74,8 +78,27 @@ fun TopBarElement(products : ProductState) {
     Scaffold(modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = { CenterAlignedTopAppBar(colors = TopAppBarDefaults.topAppBarColors(Color.White),
             title = { Image(painter = painterResource(id = R.drawable.logo), contentDescription = "Logo")},
-            navigationIcon = { IconButton(onClick = { showBottomSheet = true }) {
-                Icon(painter = painterResource(id = R.drawable.filter), contentDescription = "Filter Food", tint = Color.Black)
+            navigationIcon = {
+                Box(contentAlignment = Alignment.TopEnd) {
+                IconButton(onClick = { showBottomSheet = true }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.filter),
+                            contentDescription = "Filter Food",
+                            tint = Color.Black
+                        )
+                    }
+                    if (products.appliedTags.size > 0) {
+                        Text(text = products.appliedTags.size.toString(),
+                            color = Color.White,
+                            fontSize = 11.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.drawBehind {
+                                drawCircle(
+                                    color = Color(0xFFF15412),
+                                    radius = 25f
+                                )
+                            })
+                    }
             }},
             actions = { IconButton(onClick = { /*TODO*/ }) {
                 Icon(painter = painterResource(id = R.drawable.search), contentDescription = "Search", tint = Color.Black)
@@ -144,7 +167,9 @@ fun FilterDialog(products: ProductState): Boolean {
 
 fun ApplyTags(products: ProductState, isApplied : Boolean, tag : Int) {
     if (isApplied) {
-        products.appliedTags.add(tag)
+        if (!products.appliedTags.contains(tag)) {
+            products.appliedTags.add(tag)
+        }
     } else {
         products.appliedTags.remove(tag)
     }
